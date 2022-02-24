141f5c1c275734450a589c46d0d8020b312b4231
Anatoly <anatoly.kalugin4@yandex.ru>
2022-02-24 22:29:31 +0300

    Внесены правки в StepTracker

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -8,56 +8,56 @@ public class StepTracker {
         stepTrackerByMonth = new HashMap<>();
         Integer[] stepsJan = new Integer[30];
         autoFill(stepsJan);
-        stepTrackerByMonth.put("Январь", stepsJan);
+        stepTrackerByMonth.put("январь", stepsJan);
 
         Integer[] stepsFeb = new Integer[30];
         autoFill(stepsFeb);
-        stepTrackerByMonth.put("Февраль", stepsFeb);
+        stepTrackerByMonth.put("февраль", stepsFeb);
 
         Integer[] stepsMar = new Integer[30];
         autoFill(stepsMar);
-        stepTrackerByMonth.put("Март", stepsMar);
+        stepTrackerByMonth.put("март", stepsMar);
 
         Integer[] stepsApr = new Integer[30];
         autoFill(stepsApr);
-        stepTrackerByMonth.put("Апрель", stepsApr);
+        stepTrackerByMonth.put("апрель", stepsApr);
 
         Integer[] stepsMay = new Integer[30];
         autoFill(stepsMay);
-        stepTrackerByMonth.put("Май", stepsMay);
+        stepTrackerByMonth.put("май", stepsMay);
 
         Integer[] stepsJun = new Integer[30];
         autoFill(stepsJun);
-        stepTrackerByMonth.put("Июнь", stepsJun);
+        stepTrackerByMonth.put("июнь", stepsJun);
 
         Integer[] stepsJul = new Integer[30];
         autoFill(stepsJul);
-        stepTrackerByMonth.put("Июль", stepsJul);
+        stepTrackerByMonth.put("июль", stepsJul);
 
         Integer[] stepsAug = new Integer[30];
         autoFill(stepsAug);
-        stepTrackerByMonth.put("Август", stepsAug);
+        stepTrackerByMonth.put("август", stepsAug);
 
         Integer[] stepsSep = new Integer[30];
         autoFill(stepsSep);
-        stepTrackerByMonth.put("Сентябрь", stepsSep);
+        stepTrackerByMonth.put("сентябрь", stepsSep);
 
         Integer[] stepsOct = new Integer[30];
         autoFill(stepsOct);
-        stepTrackerByMonth.put("Октябрь", stepsOct);
+        stepTrackerByMonth.put("октябрь", stepsOct);
 
         Integer[] stepsNov = new Integer[30];
         autoFill(stepsNov);
-        stepTrackerByMonth.put("Ноябрь", stepsNov);
+        stepTrackerByMonth.put("ноябрь", stepsNov);
 
         Integer[] stepsDec = new Integer[30];
         autoFill(stepsDec);
-        stepTrackerByMonth.put("Декабрь", stepsDec);
+        stepTrackerByMonth.put("декабрь", stepsDec);
     }
 
     void saveSteps(String month, int day, int steps) {
-        if ((stepTrackerByMonth.containsKey(month)) && (day > 0) && (day <= 30) && (steps >= 0)) {
-            Integer[] addingMonth = stepTrackerByMonth.get(month);
+        if ((stepTrackerByMonth.containsKey(month.toLowerCase())) && (day > 0) && (day <= 30) && (steps >= 0)) {
+            Integer[] addingMonth = stepTrackerByMonth.get(month.toLowerCase());
             addingMonth[day - 1] = steps;
             System.out.println("Шаги сохранены!");
         } else {
@@ -67,15 +67,20 @@ public class StepTracker {
 
     void showStats(String month) {
         Converter converter = new Converter();
-        if (stepTrackerByMonth.containsKey(month)) {
-            Integer[] showingMonth = stepTrackerByMonth.get(month);
+        if (stepTrackerByMonth.containsKey(month.toLowerCase())) {
+            Integer[] showingMonth = stepTrackerByMonth.get(month.toLowerCase());
 
             showSteps(showingMonth);
             System.out.println("Всего за данный месяц было пройдено " + findSumSteps(showingMonth) + " шагов.");
-            System.out.println("В этом месяце в среднем Вы проходили " + findAverageSteps(findSumSteps(showingMonth), showingMonth.length) + " шагов в день.");
-            System.out.println("В данном месяце Вы прошагали дистанцию в " + converter.convertStepsToDistance(findSumSteps(showingMonth)) + " километров");
-            System.out.println("Также, Вы успели сжечь " + converter.convertStepsToKcal(findSumSteps(showingMonth)) + " килокалорий. Хорошая работа!");
+            System.out.println("В этом месяце в среднем Вы проходили " + findAverageSteps(findSumSteps(showingMonth),
+                                showingMonth.length) + " шагов в день.");
+            System.out.println("В данном месяце Вы прошагали дистанцию в "
+                                + converter.convertStepsToDistance(findSumSteps(showingMonth)) + " километров");
+            System.out.println("Также, Вы успели сжечь " + converter.convertStepsToKcal(findSumSteps(showingMonth))
+                                + " килокалорий. Хорошая работа!");
             System.out.println("Ваша лучшая серия составляет " + findMaxStreak(showingMonth) + " дней!");
+        } else {
+            System.out.println("Пожалуйста, проверьте правильность введенных данных и повторите попытку.");
         }
     }
 
@@ -104,16 +109,21 @@ public class StepTracker {
     }
 
     double findAverageSteps(double steps, double days) {
-        double averageSteps = steps / days;
-        return averageSteps;
+        return steps / days;
     }
 
     int findMaxStreak(Integer[] month) {
         int daysStreak = 0;
         int maxStreak = 0;
         for (int i = 0; i < month.length; i++) {
-            if (month[i] > stepGoals) {
+            if (month[i] >= stepGoals) {
                 daysStreak += 1;
+                if (i == (month.length - 1)) {
+                    if (maxStreak < daysStreak) {
+                        maxStreak = daysStreak;
+                        daysStreak = 0;
+                    }
+                }
             } else {
                 if (maxStreak < daysStreak) {
                     maxStreak = daysStreak;
